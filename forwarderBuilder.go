9b8c207689b50dcf3c7153ff1a6547c47cc0282c
Dirk007 <dirk@faustbande.de>
2022-04-13 09:27:04 +0200

    Add builder itself 🙈

---
+++ forwarderBuilder.go
@@ -0,0 +1,87 @@
+package main
+
+import "fmt"
+
+const DEFAULT_MQTT_PORT = 1883
+
+type ForwarderBuilder[V Serializeable] struct {
+	inner *MqttForwarder[V]
+}
+
+type ForwarderTopicBuilder[V Serializeable] struct {
+	ForwarderBuilder[V]
+}
+
+type ForwarderChannelBuilder[V Serializeable] struct {
+	ForwarderBuilder[V]
+}
+
+type ForwarderConnectionBuilder[V Serializeable] struct {
+	ForwarderBuilder[V]
+}
+
+func NewForwarderBuilder[V Serializeable]() *ForwarderBuilder[V] {
+	return &ForwarderBuilder[V]{
+		inner: &MqttForwarder[V]{},
+	}
+}
+
+func (builder *ForwarderBuilder[V]) Topics() *ForwarderTopicBuilder[V] {
+	return &ForwarderTopicBuilder[V]{*builder}
+}
+
+func (builder *ForwarderTopicBuilder[V]) Output(topic string) *ForwarderTopicBuilder[V] {
+	builder.inner.publishTopic = topic
+	return builder
+}
+
+func (builder *ForwarderTopicBuilder[V]) Input(topic string) *ForwarderTopicBuilder[V] {
+	builder.inner.commandTopic = topic
+	return builder
+}
+
+func (builder *ForwarderBuilder[V]) Channels() *ForwarderChannelBuilder[V] {
+	return &ForwarderChannelBuilder[V]{*builder}
+}
+
+func (builder *ForwarderChannelBuilder[V]) Data(data chan V) *ForwarderChannelBuilder[V] {
+	builder.inner.data = data
+	return builder
+}
+
+func (builder *ForwarderChannelBuilder[V]) KillWitch(killSwitch chan bool) *ForwarderChannelBuilder[V] {
+	builder.inner.killSwitch = killSwitch
+	return builder
+}
+
+func (builder *ForwarderBuilder[V]) Server() *ForwarderConnectionBuilder[V] {
+	return &ForwarderConnectionBuilder[V]{*builder}
+}
+
+func (builder *ForwarderConnectionBuilder[V]) Host(host string) *ForwarderConnectionBuilder[V] {
+	builder.inner.config.MqttHost = host
+	return builder
+}
+
+func (builder *ForwarderConnectionBuilder[V]) Port(port uint16) *ForwarderConnectionBuilder[V] {
+	builder.inner.config.MqttPort = port
+	return builder
+}
+
+func (builder *ForwarderConnectionBuilder[V]) DefaultPort() *ForwarderConnectionBuilder[V] {
+	builder.inner.config.MqttPort = DEFAULT_MQTT_PORT
+	return builder
+}
+
+func (builder *ForwarderBuilder[V]) Build() (*MqttForwarder[V], error) {
+	if builder.inner.data == nil ||
+		builder.inner.killSwitch == nil ||
+		builder.inner.publishTopic == "" ||
+		builder.inner.commandTopic == "" ||
+		builder.inner.config.MqttHost == "" ||
+		builder.inner.config.MqttPort == 0 {
+		return nil, fmt.Errorf("Builder not ready - missing settings")
+	}
+
+	return builder.inner, nil
+}
